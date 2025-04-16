2729334941fc193060a6d857db400bdabf119647
Guilherme Freire <guifidelis2004@gmail.com>
2025-04-16 20:55:38 -0300

    Criação da classe D

---
+++ D.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class D{
+    private:
+        int D1;
+        int D2;
+
+    public:
+        D(int D1, int D2){
+            this->D1 = D1;
+            this->D2 = D2;
+        }
+        
+        bool MD1(int D1){
+            return this->D1 == D1;
+        }
+
+        bool MD2(int D2){
+            return this->D2 == D2;
+        }
+};
