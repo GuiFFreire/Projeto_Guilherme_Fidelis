f88c5f1fcc999ff5cf00c2f6a3b90e4b22f08967
Guilherme Freire <guifidelis2004@gmail.com>
2025-04-16 14:01:21 -0300

    Criação da Classe C

---
+++ C.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class C{
+    private:
+        std::string C1;
+        int C2;
+    
+    public:
+
+        C(){
+            C1 = "";
+            C2 = 0;
+        }
+
+        void MC1(){
+            std::cout << "Método MC1";
+        }
+
+        void MC2(){
+            std::cout << "Método MC2";
+        }
+};
